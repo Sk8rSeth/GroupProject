59d4e82f50fa45710c343c20dbf77a108f5a9a01
Sk8rSeth <sethhowell2197@gmail.com>
2015-03-15 11:57:55 -0700

    made SQL statements safer for search

--- GroupProject/app/Models/Search.php
+++ GroupProject/app/Models/Search.php
@@ -9,29 +9,16 @@ class Search extends Model{
 	protected static $table = 'trail';
 	protected static $key = 'trail_id';
 
-	public static function getSearch($message){	
-
-		 
-				$sql = "
-						SELECT trail_id, 
-						mountain_id,
-						name, 
-						elevation_gain,
-						hashtag,
-						length,
-						description
-						FROM trail 
-						WHERE  name 
-						LIKE '%" . $message . "%' 
-						OR description
-						LIKE '%" . $message  ."%'
-						OR elevation_gain
-						LIKE '%" . $message . "%'
-					";
-				$search = DB::select($sql, ['message' => $message]);
-				// print_r($search);
-
-				return $search;
+	public static function getSearch($message){
+		$sql = "
+				SELECT trail_id, mountain_id, name, elevation_gain, hashtag, length, description
+				FROM trail 
+				WHERE  name LIKE '%' :message '%' 
+				OR description LIKE '%' :message  '%'
+				OR elevation_gain LIKE '%' :message '%'
+			";
+		$search = DB::select($sql, [':message' => $message]);
+		return $search;
 	}
 
 }
